0949370f350e94949841d5acfb729a6a36c528fe
Ryan Jackson <ryanauj@umich.edu>
2022-04-18 23:34:25 -0400

    Actually add components.

---
+++ src/components/navbar.svelte
@@ -0,0 +1,15 @@
+<style>
+  #menu {
+    padding-left: 17px;
+  }
+  #menu a {
+    font-size: 16px;
+    text-decoration: none;
+  }
+</style>
+
+<div class="w3-bar w3-black" id="menu">
+  <a href="/">Home</a> | 
+  <a href="/touchtyper">TouchTyper</a>
+</div>
+
