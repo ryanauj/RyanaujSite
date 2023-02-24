93265226f155b3f3385968ed3cc944123276e379
Ryan Jackson <ryanauj@umich.edu>
2023-02-23 17:21:41 -0800

    Initialize project.

---
+++ ryanaujsite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-*h97*+7i!*gvmdo^=xdj5ij23ahl324r*1mj0#eq+f(&@z6jgp"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
